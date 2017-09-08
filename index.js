5fad547a1e20fd2e7b3fc610b2568a3613f1126d
Robbie Greiner <robbieg@gmail.com>
2017-09-08 11:03:41 -0600

    front end works

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const words = require('fs').readFileSync("/usr/share/dict/words").toString().tri
 
 const completeMe = new CompleteMe();
 
-let suggestedWords = [];
-
 completeMe.populate(words);
 
 $('button').on('click', suggest)
+$('ul').on('click', 'li', select)
 
 function suggest() {
-  suggestedWords = [5]
-  for (var i = 0; i < 10; i++) {
-    $('ul').append("<li>" + suggestedWords[i] + "</li>");
+  $('li').remove();
+  var suggestedWords = [];
+  suggestedWords = completeMe.suggest($('input').val());
+  console.log(suggestedWords);
+  if (suggestedWords.length <= 10) {
+    for (var i = 0; i < suggestedWords.length; i++) {
+      $('ul').append("<li>" + suggestedWords[i] + "</li>");
+    }
+  } else if (suggestedWords.length > 10) {
+    for (var j = 0; j < 10; j++) {
+      $('ul').append("<li>" + suggestedWords[j] + "</li>");
+    }
   }
+}
+
+function select() {
+  completeMe.select(this.innerText);
+  $('li').remove();
+  $('input').val('')
 }
